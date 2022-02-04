383b05d49d1a3a683e248f82d1867bf417c2b617
luizgemellifilho <luizgemelli.filho@hotmail.com>
2022-02-04 16:24:13 -0300

    importa produtos

--- src/Services/Importador.php
+++ src/Services/Importador.php
@@ -7,9 +7,45 @@ use Prophetarum\BlingIntegrationPhp\Models\Produto;
 class Importador 
 {
 
+    public function getProdutos()
+    {
+        function executeGetProducts($url, $apikey){
+            $curl_handle = curl_init();
+            curl_setopt($curl_handle, CURLOPT_URL, $url . '&apikey=' . $apikey);
+            curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, TRUE);
+            $response = curl_exec($curl_handle);
+            curl_close($curl_handle);
+            return $response;
+        }
+
+        $outputType = "json";
+        $apikey = $this->apiKey;
+        $url = 'https://bling.com.br/Api/v2/produtos/' . $outputType;
+        $retorno = executeGetProducts($url, $apikey);
+        return $retorno;
+    }
+
+    public function getProduto( $code )
+    {
+        function executeGetProduct($url, $apikey){
+            $curl_handle = curl_init();
+            curl_setopt($curl_handle, CURLOPT_URL, $url . '&apikey=' . $apikey);
+            curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, TRUE);
+            $response = curl_exec($curl_handle);
+            curl_close($curl_handle);
+            return $response;
+        }
+        $apikey = $this->apiKey;
+        $outputType = "json";
+        $url = 'https://bling.com.br/Api/v2/produto/' . $code . '/' . $outputType;
+        $retorno = executeGetProduct($url, $apikey);
+        echo $retorno;
+
+    }
+
     public function getNfe()
     {
-        $apikey = "73afb574098d14a3a9c1c5fcad14cb2beecac4585888c9f5d8cfd97e74a49176b0a21a41";
+        $apikey = $this->apiKey;
         $documentNumber = 3544216;
         $documentSerie = 1;
         $outputType = "json";
@@ -46,4 +82,11 @@ class Importador
         // $retorno = executeGetProduct($url, $apikey);
         // echo $retorno;
     }
+
+    public function __construct($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    private $apiKey;
 }
